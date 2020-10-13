abbe1bc309f856603ea06db728697897a8c00016
Adam Shilling <admshng@gmail.com>
2020-10-13 16:11:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -43,5 +43,9 @@ def checkout(cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0 
-
+  counter = 0 
+  while counter < final_cart.length 
+  
+    counter += 1 
+  end 
 end
